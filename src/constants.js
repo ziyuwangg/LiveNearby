e026655e00ee3d872ec57e854dea999c830f4f4c
ZiyuWang-114 <ziyuwang1104@gmail.com>
2021-08-25 08:17:35 -0500

    upload the Home, Login, Register etc. for the complete front-end

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const TOKEN_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2Mjk3MjQ2NDksInVzZXJuYW1lIjoiamVzcyJ9.jbWZAPT9PprkCB__kRGOnMZK70MJYTiTk7dqakcswdc';
+export const BASE_URL = 'https://influential-sky-318121.uc.r.appspot.com';
+export const SEARCH_KEY = {
+    all: 0,
+    keyword: 1,
+    user: 2
+};
